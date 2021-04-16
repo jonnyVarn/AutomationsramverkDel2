3ed47f0fc824c65e470cd90dd2cb972f3532e3b6
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2021-04-16 19:54:16 +0200

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 apt install ansible -y
 apt install git -y
 git pull https://github.com/jonnyVarn/AutomationsramverkDel2.git
 cd AutomationsramverkDel2.git
+ssh-copy-id 127.0.0.1
 ansible-playbook -i hosts.yml jenkins.yml
