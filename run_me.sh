a6af16cd8561818465157f5141f6b5338e8cc8ae
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2021-04-16 20:29:03 +0200

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -5,5 +5,5 @@ git pull https://github.com/jonnyVarn/AutomationsramverkDel2.git
 cd AutomationsramverkDel2.git
 ssh-keygen -t rsa
 ssh-copy-id 127.0.0.1
-echo >> "jonny ALL=(ALL) NOPASSWD: ALL" /etc/sudoers 
+sudo echo >> "jonny ALL=(ALL) NOPASSWD: ALL" /etc/sudoers 
 ansible-playbook -i hosts.yml jenkins.yml
